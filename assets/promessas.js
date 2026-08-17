925123e61a0b85bca990e857f8b631ab4b2ed16b
midmake <midasstudiobr@gmail.com>
2026-08-17 17:57:54 -0300

    Separa dados de desenvolvimento das Promessas

---
+++ assets/promessas.js
@@ -0,0 +1,14 @@
+export const promessas = [
+  {
+    id: "dev-001",
+    texto: "Conteúdo bíblico de desenvolvimento.",
+    livro: "",
+    capitulo: null,
+    versiculoInicial: null,
+    versiculoFinal: null,
+    referencia: "Referência será definida",
+    traducao: "Tradução a definir",
+    categoria: "desenvolvimento",
+    ativo: true
+  }
+];
